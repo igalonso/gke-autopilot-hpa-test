b89c97a6648d3f7297f0db466206876ea9bf0ae5
Ignacio García <igalonso@gmail.com>
2022-05-23 11:04:28 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
-export MY_IP='34.66.84.160'
+export MY_IP='34.136.194.98'
+
 kubectl autoscale deployment/web --min=2 --max=15 --cpu-percent=50
-docker run -e 'POST=vote=a' -it --rm -t devth/alpine-bench -c50 -n200000 http://$MY_IP/
+
+docker run -e 'POST=vote=a' -it --rm -t devth/alpine-bench -c30 -n20000 http://$MY_IP/
+docker run -e 'POST=vote=a' -it --rm -t devth/alpine-bench -c50 -n30000 http://$MY_IP/
+docker run -e 'POST=vote=a' -it --rm -t devth/alpine-bench -c60 -n60000 http://$MY_IP/
+docker run -e 'POST=vote=a' -it --rm -t devth/alpine-bench -c100 -n100000 http://$MY_IP/
